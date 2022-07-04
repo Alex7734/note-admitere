a20d192d9f9e1a76c6c9adbd29c445a5b87e89bb
Alex7734 <63779353+Alex7734@users.noreply.github.com>
2022-07-04 12:30:25 +0300

    Add files via upload

--- analizeaza.py
+++ analizeaza.py
@@ -19,6 +19,7 @@ for item in vector:
 
 sortedData = sorted([float(item) for item in filtrat])
 x=np.array(sortedData)
+zece=sortedData.count(10)
 
 plt.figure(figsize=(10,6))
 plt.hist(x, bins=20, density=True, color='c', edgecolor='k', alpha=0.7)
@@ -27,6 +28,7 @@ plt.axvline(np.median(x), color='g', linestyle='dashed', linewidth=2, label='Med
 plt.axvline(mediaAlex, color='r', linestyle='solid', linewidth=1, label='Media lui Alex')
 plt.xlabel("Nota admitere buget Calculatoare 2021 (327 de locuri)", fontsize=14)
 plt.ylabel("Numar relativ de note", fontsize=12)
-plt.title(f"Median:{np.median(x)} ||  Media generala:{x.mean():.2f} || Ultima nota:{x[0]}")
+plt.title(f"Median:{np.median(x)} ||  Media generala:{x.mean():.2f} || Ultima nota:{x[0]} || {zece} de note cu 10")
 plt.legend(loc="upper left")
 plt.show()
+print(zece)
